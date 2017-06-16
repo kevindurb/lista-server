e0e55ef781cc81429f22bf017fb5fb552864c3fb
Kevin Durbin <kevindurb@gmail.com>
2017-06-16 07:31:26 -0600

    Rename for better naming

--- db/users.js
+++ db/users.js
@@ -1,13 +1,13 @@
-const connect = require('./connect');
+const pool = require('./pool');
 const helpers = require('./helpers');
 
 module.exports = {
   getUserById(id) {
-    return connect.query('select * from users where id = $1', [id])
+    return pool.query('select * from users where id = $1', [id])
       .then(helpers.firstRow);
   },
   getUserByUsername(username) {
-    return connect.query('select * from users where username = $1', [username])
+    return pool.query('select * from users where username = $1', [username])
       .then(helpers.firstRow);
   }
 };
