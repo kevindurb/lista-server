b651964e4de9e78199fecf1f65751844d06e7e07
Kevin Durbin <kevindurb@gmail.com>
2017-06-17 15:29:01 -0600

    Add get me route

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,5 +3,6 @@ const thenSend = require('../../utils/thenSend');
 
 router.post('/', thenSend(require('./login')));
 router.delete('/', thenSend(require('./logout')));
+router.get('/', thenSend(require('./me')));
 
 module.exports = router;
---
+++ src/routes/auth/me.js
@@ -0,0 +1,21 @@
+const responses = require('../../utils/responses');
+const session = require('../../utils/session');
+const userPresenter = require('../../presenters/user');
+
+module.exports = (req) => {
+  const db = req.db;
+
+  if (session.isLoggedOut(req)) {
+    return responses.notAuthorized();
+  }
+
+  const id = session.getCurrentUser(req).id;
+
+  return db.users.getById(id)
+  .then(result => {
+    req.session.user = result;
+    return responses.success(
+      userPresenter(result)
+    );
+  });
+};
