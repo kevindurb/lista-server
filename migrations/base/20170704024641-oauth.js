a5ce3ff5c0b4a3c2c5ee68e1ef02878a7e63899d
Kevin Durbin <kevindurb@gmail.com>
2017-07-03 21:06:00 -0600

    Add oauth tables

---
+++ migrations/base/20170704024641-oauth.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = db => (
+  Promise.all([
+    db.createTable('oauth_tokens', {
+      id: {
+        type: 'uuid',
+        primaryKey: true,
+        defaultValue: new String('uuid_generate_v4()'),
+      },
+      client_id: 'uuid',
+      user_id: 'uuid',
+      access_token: 'text',
+      access_token_expires_at: 'datetime',
+      refresh_token: 'text',
+      refresh_token_expires_at: 'datetime',
+      created_at: {
+        type: 'datetime',
+        defaultValue: new String('now()'),
+      },
+      updated_at: {
+        type: 'datetime',
+        defaultValue: new String('now()'),
+      },
+    }),
+    db.createTable('oauth_clients', {
+      id: {
+        type: 'uuid',
+        primaryKey: true,
+        defaultValue: new String('uuid_generate_v4()'),
+      },
+      name: {
+        type: 'text',
+        unique: true,
+      },
+      secret: {
+        type: 'uuid',
+        unique: true,
+        defaultValue: new String('uuid_generate_v4()'),
+      },
+      redirect_uri: 'text',
+      created_at: {
+        type: 'datetime',
+        defaultValue: new String('now()'),
+      },
+      updated_at: {
+        type: 'datetime',
+        defaultValue: new String('now()'),
+      },
+    })
+  ])
+);
+
+exports.down = db => (
+  db.dropTable('users')
+);
+
+exports._meta = {
+  "version": 1
+};
